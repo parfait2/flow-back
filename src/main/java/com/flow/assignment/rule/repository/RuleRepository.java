6894009624a9c6f03505e29e714c09ce118cb4d9
parfait2 <parkyl0208@naver.com>
2024-08-16 17:17:46 +0900

    fix: Wrong import reference

--- src/main/java/com/flow/assignment/rule/repository/RuleRepository.java
+++ src/main/java/com/flow/assignment/rule/repository/RuleRepository.java
@@ -1,6 +1,6 @@
 package com.flow.assignment.rule.repository;
 
-import org.apache.tomcat.util.digester.Rule;
+import com.flow.assignment.rule.entity.Rule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RuleRepository extends JpaRepository<Rule, Long> {
